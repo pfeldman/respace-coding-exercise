3caaedb9818d5e5379e0f3138ebbf87f113f6c94
Pablo Feldman <pablofeldman90@gmail.com>
2021-11-30 22:09:26 -0300

    :bug: fix: Fix issue on vercel, mounting some packages

--- pages/seeker.tsx
+++ pages/seeker.tsx
@@ -1,20 +1,15 @@
 import type { NextPage } from 'next'
 import {
-  DesktopDatePicker,
-  MobileDatePicker,
-  LocalizationProvider,
-} from '@mui/lab'
-import AdapterDateFns from '@mui/lab/AdapterDateFns'
-import {
   Box,
   Button,
   Grid,
   TextField,
-  TextFieldProps,
   Typography,
   Modal,
   Snackbar,
 } from '@mui/material'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 import { useEffect, useState } from 'react'
 import { DAYS_WEEK_NAME } from 'resources/constants'
 import { useData } from 'hooks/useData'
@@ -37,7 +32,7 @@ const Seeker: NextPage = () => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: [300, 400],
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -125,18 +120,8 @@ const Seeker: NextPage = () => {
 
   const hasAvailableTime = !!dayAvailability.find((hour) => hour.available)
 
-  const handleChange = (date: string | null) => {
-    setDate(date ?? '')
-  }
-
-  const datePickerProps = {
-    label: 'Meeting day',
-    inputFormat: 'MM/dd/yyyy',
-    value: date,
-    onChange: handleChange,
-    renderInput: (params: TextFieldProps) => <TextField {...params} />,
-    // eslint-disable-next-line
-    minDate: new Date() as any,
+  const handleChange = (date: Date) => {
+    setDate(date.toISOString())
   }
 
   const numberToTime = (value: number) => {
@@ -176,112 +161,102 @@ const Seeker: NextPage = () => {
   }
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <Box>
+      <Typography>Select a date to meet with the provider:</Typography>
       <Box>
-        <Typography>Select a date to meet with the provider:</Typography>
-        <Box
-          sx={{
-            display: ['none', 'block'],
-          }}
-        >
-          <DesktopDatePicker {...datePickerProps} />
-        </Box>
-        <Box
-          sx={{
-            display: ['block', 'none'],
-          }}
-        >
-          <MobileDatePicker {...datePickerProps} />
-        </Box>
-        {date && !hasAvailableTime && (
-          <Typography
-            sx={{
-              fontStyle: 'italic',
-              fontSize: '12px',
-              color: 'secondary.200',
-            }}
-          >
-            There are no open slots. Please try a different date.
-          </Typography>
-        )}
-
-        {date && hasAvailableTime && (
-          <>
-            <Typography sx={{ mt: 2 }}>
-              Please choose the time that better matches for you:
-            </Typography>
-            <Grid container spacing={2}>
-              {dayAvailability.map((hour, index) => {
-                if (hour.available) {
-                  return (
-                    <Grid item xs={6} md={4} key={index}>
-                      <Button
-                        sx={{ width: '100%' }}
-                        variant="contained"
-                        onClick={() => {
-                          setModalOpen(true)
-                          setMeetingTime(index)
-                        }}
-                      >
-                        {numberToTime(index)} - {numberToTime(index + 1)}{' '}
-                        -&nbsp;<strong>${hour.price}</strong>
-                      </Button>
-                    </Grid>
-                  )
-                }
-
-                return null
-              })}
-            </Grid>
-          </>
-        )}
-
-        <Modal
-          open={modalOpen}
-          onClose={() => setModalOpen(false)}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={modalStyles}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Confirm meeting
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2, mb: 3 }}>
-              You would be meeting from {numberToTime(meetingTime)} to{' '}
-              {numberToTime(meetingTime + 1)} on {getDateFormatted(date)}
-            </Typography>
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'space-around',
-                alignItems: 'center',
-              }}
-            >
-              <TextField
-                error={nameError}
-                helperText={nameError ? 'You must enter a name' : undefined}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                label="Your Name"
-              />
-              <Button
-                variant="contained"
-                onClick={() => handleTakeTurn(meetingTime)}
-              >
-                Confirm
-              </Button>
-            </Box>
-          </Box>
-        </Modal>
-        <Snackbar
-          open={snackOpen}
-          autoHideDuration={6000}
-          onClose={handleClose}
-          message="Your Turn was saved"
-          action={<SnackBarAction handleClose={() => handleClose()} />}
+        <DatePicker
+          selected={date ? new Date(date) : null}
+          onChange={handleChange}
         />
       </Box>
-    </LocalizationProvider>
+      {date && !hasAvailableTime && (
+        <Typography
+          sx={{
+            fontStyle: 'italic',
+            fontSize: '12px',
+            color: 'secondary.200',
+          }}
+        >
+          There are no open slots. Please try a different date.
+        </Typography>
+      )}
+
+      {date && hasAvailableTime && (
+        <>
+          <Typography sx={{ mt: 2 }}>
+            Please choose the time that better matches for you:
+          </Typography>
+          <Grid container spacing={2}>
+            {dayAvailability.map((hour, index) => {
+              if (hour.available) {
+                return (
+                  <Grid item xs={6} md={4} key={index}>
+                    <Button
+                      sx={{ width: '100%' }}
+                      variant="contained"
+                      onClick={() => {
+                        setModalOpen(true)
+                        setMeetingTime(index)
+                      }}
+                    >
+                      {numberToTime(index)} - {numberToTime(index + 1)} -&nbsp;
+                      <strong>${hour.price}</strong>
+                    </Button>
+                  </Grid>
+                )
+              }
+
+              return null
+            })}
+          </Grid>
+        </>
+      )}
+
+      <Modal
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyles}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Confirm meeting
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2, mb: 3 }}>
+            You would be meeting from {numberToTime(meetingTime)} to{' '}
+            {numberToTime(meetingTime + 1)} on {getDateFormatted(date)}
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+            }}
+          >
+            <TextField
+              error={nameError}
+              helperText={nameError ? 'You must enter a name' : undefined}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              label="Your Name"
+            />
+            <Button
+              variant="contained"
+              onClick={() => handleTakeTurn(meetingTime)}
+            >
+              Confirm
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+      <Snackbar
+        open={snackOpen}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Your Turn was saved"
+        action={<SnackBarAction handleClose={() => handleClose()} />}
+      />
+    </Box>
   )
 }
 
